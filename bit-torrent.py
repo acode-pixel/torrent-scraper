e2270bca881d040e880f6e668ca7a6a8799fac41
acode.pixel <donaldanthony245@gmail.com>
2024-07-18 10:50:47 +0800

    modified:   bit-torrent.py

--- bit-torrent.py
+++ bit-torrent.py
@@ -13,19 +13,33 @@ class torrentFile:
         return f"{self.info_hash} {self.name}"
     
 def getScapeData(scrapeURL: str):
+    print(f"Trying scrape url: {scrapeURL}")
     URL = urlparse(scrapeURL)
     if URL.scheme == "http" or URL.scheme == "https":
             
         try:
-            r = requests.get(scrapeURL)
+            r = requests.get(scrapeURL, verify=False)
             r.raise_for_status()
+            
+            if args.verbose:
+                print(f"RAW_CONTENT: {r.content.decode()}")
+                print(f"SCRAPE_CONTENT: {bencodepy.decode(r.content)}")
+            
             scrapeData = bencodepy.decode(r.content)
             file_hashes = list(scrapeData[b"files"].keys())
             file_stats = list(scrapeData[b"files"].values())
             hash_index = 0
             
+            if args.verbose:
+                print(f"FILE_STATS: {file_stats}\nFILE_HASHES: {file_hashes}")
+            
             for i in file_stats:
-                torrentFiles.append(torrentFile(i[b"name"].decode("utf-8"),
+                try:
+                    name = (i[b"name"]).decode("utf-8")
+                except Exception:
+                    name = "Unknown"
+                    
+                torrentFiles.append(torrentFile(name,
                                                 i[b"downloaded"],
                                                 i[b"complete"],
                                                 i[b"incomplete"],
@@ -35,8 +49,13 @@ def getScapeData(scrapeURL: str):
             for i in torrentFiles:
                 print(i)
 
+            print(f"Total files: {len(torrentFiles)}")
 
-        except requests.exceptions.RequestException as error:
+        except Exception as error:
+            if type(error).__name__ == "HTTPError":
+                getScapeData(scrapeURL.replace("http://", "https://"))
+                exit()
+                
             print("Failed to get scrape info.", type(error).__name__)
             exit()
     elif URL.scheme == "udp":
@@ -50,67 +69,79 @@ def getScapeData(scrapeURL: str):
         print(f"Unknown protocol {URL.scheme}")
         exit()    
 
+def fileOutput(URL):
+    fields = ["filename", "info_hash", "seeders", "leechers", "downloads"]
+    filename = f"{time.strftime('%H-%M-%S')}-{URL.hostname}"
+
+    if args.xlsx:
+        # xlsx output
+        workbook = xlsxwriter.Workbook(f'{filename}.xlsx')
+        worksheet = workbook.add_worksheet()
+        bold = workbook.add_format({'bold': True})
+
+        col = 0
+        for i in fields:
+            worksheet.write(0, col, i)
+            col += 1
+                
+        col = 0
+        row = 1
+        for i in torrentFiles:
+            worksheet.write(row, col, i.name)
+            worksheet.write(row, col+1, i.info_hash)
+            worksheet.write(row, col+2, i.complete)
+            worksheet.write(row, col+3, i.incomplete)
+            worksheet.write(row, col+4, i.downloaded)
+            row += 1
+                
+        worksheet.write(row, 0, "Total files", bold)
+        worksheet.write(row, 4, len(torrentFiles), bold)
+        workbook.close()
+
+    elif args.csv:
+        # csv output
+        with open(filename+".csv", "w", newline='') as f:
+            csvwriter = csv.writer(f)
+            csvwriter.writerow(fields)
+            for i in torrentFiles:
+                csvwriter.writerow([i.name, i.info_hash, i.complete, i.incomplete, i.downloaded])
+            csvwriter.writerow([f"Total files: {len(torrentFiles)}"])
+            f.close()
+
 if __name__ ==  "__main__" :
     parser = argparse.ArgumentParser(description="torrent parser")
-    parser.add_argument("torrent_file", help="torrent file to parse")
+    parser.add_argument("-f", "--torrent_file", help="torrent file to parse")
+    parser.add_argument("-a", "--announce_uri", help="use uri as announce")
     parser.add_argument("--csv", action="store_true", help="store output in csv file")
     parser.add_argument("--xlsx", action="store_true", help="store output in xlsx file")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Output verbose")
 
     args = parser.parse_args()
-
-    with open(args.torrent_file, "rb") as f:
-        metainfo = bencodepy.decode(f.read())
-        announceURL = metainfo[b"announce"].decode("utf-8")
-        scrapeURL = announceURL.replace("announce", "scrape")
-        print(f"scrape: {scrapeURL}")
-
+    
+    if args.announce_uri != None:
+        scrapeURL = args.announce_uri.replace("announce", "scrape")
+        
         URL = urlparse(scrapeURL)
         torrentFiles = []
 
         getScapeData(scrapeURL)
-
-            
-        fields = ["filename", "info_hash", "seeders", "leechers", "downloads"]
-        filename = f"{time.strftime('%H-%M-%S')}-{URL.hostname}"
-
-        if args.xlsx:
-            # xlsx output
-            workbook = xlsxwriter.Workbook(f'{filename}.xlsx')
-            worksheet = workbook.add_worksheet()
-            bold = workbook.add_format({'bold': True})
-
-            col = 0
-            for i in fields:
-                worksheet.write(0, col, i)
-                col += 1
-                
-            col = 0
-            row = 1
-            for i in torrentFiles:
-                worksheet.write(row, col, i.name)
-                worksheet.write(row, col+1, i.info_hash)
-                worksheet.write(row, col+2, i.complete)
-                worksheet.write(row, col+3, i.incomplete)
-                worksheet.write(row, col+4, i.downloaded)
-                row += 1
-                
-            worksheet.write(row, 0, "Total files", bold)
-            worksheet.write(row, 4, len(torrentFiles), bold)
-            workbook.close()
-
-        elif args.csv:
-            # csv output
-            with open(filename+".csv", "w", newline='') as f:
-                csvwriter = csv.writer(f)
-                csvwriter.writerow(fields)
-                for i in torrentFiles:
-                    csvwriter.writerow([i.name, i.info_hash, i.complete, i.incomplete, i.downloaded])
-                csvwriter.writerow([f"Total files: {len(torrentFiles)}"])
-                f.close()
-
-
-
-
+        fileOutput(URL)
         
         
+    elif args.torrent_file != None:
+        with open(args.torrent_file, "rb") as f:
+            metainfo = bencodepy.decode(f.read())
+            try:
+                announceURL = metainfo[b"announce"].decode("utf-8")
+                scrapeURL = announceURL.replace("announce", "scrape")
+                print(f"scrape: {scrapeURL}")
 
+                URL = urlparse(scrapeURL)
+                torrentFiles = []
+
+                getScapeData(scrapeURL)
+                fileOutput(URL)
+                
+            except Exception as error:
+                print("Failed to get scrape info.", type(error).__name__)
+                exit()
